c9078df8ad7cb95bc53d06c1b15b622342cd1a1c
sajio1 <71483124+sajio1@users.noreply.github.com>
2024-09-16 09:57:37 -0400

    Update MyAI.py new algorithm

--- Minesweeper_Python/src/MyAI.py
+++ Minesweeper_Python/src/MyAI.py
@@ -14,81 +14,121 @@
 
 from AI import AI
 from Action import Action
+import random
+
+class MyAI(AI):
+    def __init__(self, rowDimension, colDimension, totalMines, startX, startY):
+        self.rowDimension = rowDimension
+        self.colDimension = colDimension
+        self.totalMines = totalMines
+        self.startX = startX
+        self.startY = startY
+
+        self.board = []
+        for _ in range(rowDimension):
+            row = []
+            for _ in range(colDimension):
+                row.append(-1)
+            self.board.append(row)
+
+        self.cellsToUncover = [(startX, startY)]
+        self.flaggedCells = set()
+        self.safeCellsToUncover = set()
+        self.processedCells = set()
+        self.minesInfo = {}
 
 
-class MyAI( AI ):
+    def getAction(self, numAdjacentMines: int):
+        if self.cellsToUncover:
+            currentX, currentY = self.cellsToUncover.pop(0)
+            self.board[currentX][currentY] = numAdjacentMines
+            self.processedCells.add((currentX, currentY))
+            self.updateMinesInfo(currentX, currentY, numAdjacentMines)
 
-	def __init__(self, rowDimension, colDimension, totalMines, startX, startY):
+        self.getSafeAndFlags()
 
-		########################################################################
-		#							YOUR CODE BEGINS						   #
-		########################################################################
-		# initialize the AI's knowledge space
-		self.rowDimension = rowDimension
-		self.colDimension = colDimension
-		self.totalMines = totalMines
-		self.startX = startX
-		self.startY = startY
+        if self.safeCellsToUncover:
+            nextX, nextY = self.safeCellsToUncover.pop()
+            if (nextX, nextY) not in self.processedCells:
+                self.cellsToUncover.append((nextX, nextY))
+                return Action(AI.Action.UNCOVER, nextX, nextY)
 
-		# keep tracking uncover cells and their corresponding numbers
-		self.board = [[-1 for _ in range(colDimension)] for _ in range(rowDimension)]
-		self.board[startX][startY] = 0
+        for cell in self.flaggedCells:
+            if self.board[cell[0]][cell[1]] != 9:
+                self.board[cell[0]][cell[1]] = 9
+                return Action(AI.Action.FLAG, cell[0], cell[1])
 
-		# Track cells to uncover or flag
-		self.uncoverQueue = [(startX, startY)]
-		self.flagged = set()
-		self.safeToUncover = set()
-		self.movesMade = set()
-		########################################################################
-		#							YOUR CODE ENDS							   #
-		########################################################################
+        nextMove = self.getBestMove()
+        if nextMove:
+            self.cellsToUncover.append(nextMove)
+            return Action(AI.Action.UNCOVER, nextMove[0], nextMove[1])
 
-		
-	def getAction(self, number: int) -> "Action Object":
+        return Action(AI.Action.LEAVE)
 
-		########################################################################
-		#							YOUR CODE BEGINS						   #
-		########################################################################
-		# Update the board with the given number for the last uncovered cell
-		if self.uncoverQueue:
-			x, y = self.uncoverQueue.pop(0)
-			self.board[x][y] = number
-			self.movesMade.add((x, y))
 
-			if number == 0:
-				for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)]:
-					nx, ny = x + dx, y + dy
-					if 0 <= nx < self.rowDimension and 0 <= ny < self.colDimension and self.board[nx][ny] == -1:
-						self.safeToUncover.add((nx, ny))
+    def updateMinesInfo(self, x, y, numAdjacentMines):
+        self.minesInfo[(x, y)] = numAdjacentMines
 
-		if self.safeToUncover:
-			x, y = self.safeToUncover.pop()
-			if (x, y) not in self.movesMade:
-				self.uncoverQueue.append((x, y))
-				return Action(AI.Action.UNCOVER, x, y)
+        if numAdjacentMines == 0:
+            for deltaX, deltaY in [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)]:
+                neighborX, neighborY = x + deltaX, y + deltaY
+                if 0 <= neighborX < self.rowDimension and 0 <= neighborY < self.colDimension and self.board[neighborX][neighborY] == -1:
+                    self.safeCellsToUncover.add((neighborX, neighborY))
 
-		# Flag the mine 
-		for i in range(self.rowDimension):
-			for j in range(self.colDimension):
-				if self.board[i][j] == -1 and self.totalMines == 1 and (i, j) not in self.flagged:
-					self.flagged.add((i, j))
-					return Action(AI.Action.FLAG, i, j)
 
-		# If no certain moves, random guess
-		for i in range(self.rowDimension):
-			for j in range(self.colDimension):
-				if self.board[i][j] == -1 and (i, j) not in self.flagged and (i, j) not in self.movesMade:
-					self.uncoverQueue.append((i, j))
-					return Action(AI.Action.UNCOVER, i, j)
+    def getSafeAndFlags(self):
+        for x in range(self.rowDimension):
+            for y in range(self.colDimension):
+                numAdjacentMines = self.board[x][y]
+                if numAdjacentMines not in [-1, 9, 0]:
+                    coveredCells, flaggedCells = self.getCoveredAndFlagged(x, y)
+                    if flaggedCells == numAdjacentMines and coveredCells:
+                        for coveredX, coveredY in coveredCells:
+                            self.safeCellsToUncover.add((coveredX, coveredY))
+                    elif flaggedCells + len(coveredCells) == numAdjacentMines:
+                        for coveredX, coveredY in coveredCells:
+                            self.flaggedCells.add((coveredX, coveredY))
 
-		return Action(AI.Action.LEAVE)
-		# uncover the next cell in the queue
-		if self.uncoverQueue:
-			x, y = self.uncoverQueue.pop(0)
-			return Action(AI.Action.UNCOVER, x, y)
 
-		
-		return Action(AI.Action.LEAVE)
-		########################################################################
-		#							YOUR CODE ENDS							   #
-		########################################################################
+    def getCoveredAndFlagged(self, x, y):
+        coveredCells = []
+        flaggedCells = 0
+        for deltaX, deltaY in [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)]:
+            neighborX, neighborY = x + deltaX, y + deltaY
+            if 0 <= neighborX < self.rowDimension and 0 <= neighborY < self.colDimension:
+                if self.board[neighborX][neighborY] == -1:
+                    coveredCells.append((neighborX, neighborY))
+                elif (neighborX, neighborY) in self.flaggedCells:
+                    flaggedCells += 1
+        return coveredCells, flaggedCells
+
+
+    def getBestMove(self):
+        minProb = float('inf')
+        bestMove = None
+        for x in range(self.rowDimension):
+            for y in range(self.colDimension):
+                if self.board[x][y] == -1:
+                    prob = self.getMineProb(x, y)
+                    if prob < minProb:
+                        minProb = prob
+                        bestMove = (x, y)
+        return bestMove
+
+
+    def getMineProb(self, x, y):
+        prob = 0
+        weightSum = 0
+        for deltaX, deltaY in [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)]:
+            neighborX, neighborY = x + deltaX, y + deltaY
+            if 0 <= neighborX < self.rowDimension and 0 <= neighborY < self.colDimension:
+                if (neighborX, neighborY) in self.minesInfo:
+                    numAdjacentMines = self.minesInfo[(neighborX, neighborY)]
+                    coveredCells, flaggedCells = self.getCoveredAndFlagged(neighborX, neighborY)
+                    if coveredCells:
+                        distanceWeight = 1 / (abs(x - neighborX) + abs(y - neighborY))
+                        prob += (numAdjacentMines - flaggedCells) / len(coveredCells) * distanceWeight
+                        weightSum += distanceWeight
+        if weightSum == 0:
+            return float('inf')
+        return prob / weightSum
